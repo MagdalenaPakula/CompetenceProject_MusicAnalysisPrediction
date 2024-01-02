39b4a1c5548aabe6edf710c31e620ba489abb42e
arturpietrzak <artur.pietrzak1@outlook.com>
2024-01-02 02:41:57 +0100

    Fix json serialization

--- app/server/app.py
+++ app/server/app.py
@@ -35,7 +35,8 @@ def search():
     results = spotify.search(q="track:" + title, type='track')
 
     response = jsonify({
-        'tracks': map(lambda t : {'name': t["name"], authors: ", ".join(map(lambda a : a["name"], t["artists"]["items"]))},results["tracks"]["items"])})
+        'tracks': list(map(lambda t : {'name': t["name"], authors: ", ".join(list(map(lambda a : a["name"], t["artists"]["items"])))},results["tracks"]["items"]))
+        })
 
     return results["tracks"]["items"][0]["name"] + str(results["tracks"]["items"][0]["popularity"])
 
