7028aa9e501d2fb37cdc5d2c85a04ab9dd991f46
arturpietrzak <artur.pietrzak1@outlook.com>
2024-01-02 01:17:06 +0100

    Add text

--- app/server/app.py
+++ app/server/app.py
@@ -31,7 +31,7 @@ def search():
 
     r = requests.get("https://api.spotify.com/v1/search", params={'type': 'track', 'q': title}, headers={'Authorization': 'Basic ZDQ1ODAyNTZiNGMxNGQ4Yjk1YmQ1MWViZGRhYTQ5MzI6Y2NlMjI0YTQ2OTMwNDU2ZjgyNzQwNmJkZmQ4YjAzNTk='})
 
-    return r
+    return r.text
 
 
 @app.route('/api/predict_popularity', methods=['POST'])
